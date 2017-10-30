3092d0ddf00be556aa8518969704cc0b12255c2f
Juan Larosa <juanlarosa96@gmail.com>
2017-10-29 22:30:24 -0300

    Agrego funciones de Potencial

---
+++ TrabajoPractico/FuncionesPotencial.m
@@ -0,0 +1,19 @@
+addpath(pwd)
+function  resultado = potencial(sumalnX,sumalnY, sumalnXCuadrados, productolnXY, N) %Calcula a y b para la aproximacion potencial    
+    a = det([productolnXY, sumalnX; sumalnY, N]) / det([sumalnXCuadrados, sumalnX; sumalnX, N]);
+    B = det([sumalnXCuadrados, productolnXY; sumalnX, sumalnY]) / det([sumalnXCuadrados, sumalnX; sumalnX, N]);
+    b = e^B;
+    resultado = [a,b];
+endfunction
+
+
+function graficarPotencial(x1, x2, a, b)
+  x = x1:0.1:x2;
+  y = b * (x.^a);
+  plot(x,y);  
+endfunction
+
+
+res = potencial(9.6, 26.7, 21.1, 58.51, 5);
+printf("Y = %de^(%dX)\n", res(2), res(1));
+graficarPotencial(1, 2.9, res(1), res(2));
