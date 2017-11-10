2dcc0087a8f20ab4b006622931d76745d9f34be3
Juan Larosa <juanlarosa96@gmail.com>
2017-11-10 00:56:54 -0300

    Agrego calculo de errores

--- TrabajoPractico/TablaComparaciones.m
+++ TrabajoPractico/TablaComparaciones.m
@@ -21,7 +21,7 @@ function mostrarTabla(boolLineal, boolParabola, boolExponencial, boolPotencial, 
       res = lineal(sumaVector(x), sumaVector(y), sumaCuadradosVector(x), sumaVectorProductoXY(x, y), length(x),decimal);
       for i = 1:length(x)        
       uicontrol (f, "style", "text", "string",num2str(funcionRedondeo(res(1)*x(i) + res(2), decimal)) , "position",[margen (altura-i*50) 80 30]);
-      erroresLineal(i) = abs(funcionRedondeo(y(i) - res(1)*x(i) + res(2), decimal));
+      erroresLineal(i) = abs(y(i) - funcionRedondeo((res(1)*x(i) + res(2)), decimal));
     endfor
     margen = margen + 100;
   endif
@@ -30,6 +30,7 @@ function mostrarTabla(boolLineal, boolParabola, boolExponencial, boolPotencial, 
       res = parabola(length(x),sumaVector(x),sumaVector(y),sumaCuadradosVector(x),sumaCuboVector(x),sumaCuartaVector(x),sumaVectorProductoXY(x,y),sumaVectorProductoX2Y(x,y),decimal);
       for i = 1:length(x)        
       uicontrol (f, "style", "text", "string",num2str(funcionRedondeo(res(1)*x(i)*x(i) + res(2) *x(i) + res(3), decimal)) , "position",[margen (altura-i*50) 80 30]);
+      erroresParabola(i) = abs(funcionRedondeo(y(i) - (res(1)*x(i)*x(i) + res(2) *x(i) + res(3)), decimal));
     endfor
     margen = margen + 100;
   endif
@@ -40,6 +41,7 @@ function mostrarTabla(boolLineal, boolParabola, boolExponencial, boolPotencial, 
       for i = 1:length(x)        
       uicontrol (f, "style", "text", "string",num2str(funcionRedondeo(res(2)*e ^(x(i)*res(1)), decimal)) , "position",[margen (altura-i*50) 80 30]);
     endfor
+    erroresExponencial(i) = abs(y(i) - (funcionRedondeo(res(2)*e ^(x(i)*res(1)), decimal)));
     margen = margen + 100;
   endif
   
@@ -48,6 +50,7 @@ function mostrarTabla(boolLineal, boolParabola, boolExponencial, boolPotencial, 
       res = hiperbolica(length(x),sumaVector(x),sumaVector(y),sumaVector1X(x),sumaVector1X(y),sumaCuadrados1XVector(x),sumaVectorProducto1X1Y(x,y),decimal);
       for i = 1:length(x)        
       uicontrol (f, "style", "text", "string",num2str(funcionRedondeo(res(1)/(x(i)*res(2)), decimal)) , "position",[margen (altura-i*50) 80 30]);
+      erroresHiperbola(i) = abs(y(i) - funcionRedondeo(res(1)/(x(i)*res(2)), decimal));
     endfor
     margen = margen + 100;
   endif
@@ -57,9 +60,57 @@ function mostrarTabla(boolLineal, boolParabola, boolExponencial, boolPotencial, 
       res =  potencial(sumaLogaritmo(x),sumaLogaritmo(y),sumaLogaritmoCuadrado(x),sumaVectorProductoLnXLnY(x,y),length(x),decimal);
       for i = 1:length(x)        
       uicontrol (f, "style", "text", "string",num2str(funcionRedondeo(res(2)*x(i) ^(x(i)*res(1)), decimal)) , "position",[margen (altura-i*50) 80 30]);
+      erroresPotencial(i) = abs(y(i) - funcionRedondeo(res(2)*x(i) ^(x(i)*res(1)), decimal));
     endfor
     margen = margen + 100;
   endif
   
+  %Errores
+  
+  if(boolLineal)
+      uicontrol (f, "style", "text", "string", "Error Lineal", "position",[margen altura 100 30]);      
+      for i = 1:length(x)        
+      uicontrol (f, "style", "text", "string",num2str(erroresLineal(i)) , "position",[margen (altura-i*50) 80 30]);     
+    endfor
+    uicontrol (f, "style", "text", "string",num2str(sum(erroresLineal)) , "position",[margen (altura-(length(x)+1)*50) 80 30]);
+    margen = margen + 120;
+  endif
+  
+   if(boolParabola)
+      uicontrol (f, "style", "text", "string", "Error Parabola", "position",[margen altura 100 30]);      
+      for i = 1:length(x)        
+      uicontrol (f, "style", "text", "string",num2str(erroresParabola(i)) , "position",[margen (altura-i*50) 80 30]);      
+    endfor
+    uicontrol (f, "style", "text", "string",num2str(sum(erroresParabola)) , "position",[margen (altura-(length(x)+1)*50) 80 30]);
+    margen = margen + 120;
+  endif
+  
+  if(boolExponencial)
+      uicontrol (f, "style", "text", "string", "Error Exponencial", "position",[margen altura 100 30]);      
+      for i = 1:length(x)        
+      uicontrol (f, "style", "text", "string",num2str(erroresExponencial(i)) , "position",[margen (altura-i*50) 80 30]);      
+    endfor
+    uicontrol (f, "style", "text", "string",num2str(sum(erroresExponencial)) , "position",[margen (altura-(length(x)+1)*50) 80 30]);
+    margen = margen + 120;
+  endif  
+ 
+  
+  if(boolHiperbola)
+      uicontrol (f, "style", "text", "string", "Error Hiperbola", "position",[margen altura 100 30]);      
+      for i = 1:length(x)        
+      uicontrol (f, "style", "text", "string",num2str(erroresHiperbola(i)) , "position",[margen (altura-i*50) 80 30]);      
+    endfor
+    uicontrol (f, "style", "text", "string",num2str(sum(erroresHiperbola)) , "position",[margen (altura-(length(x)+1)*50) 80 30]);
+    margen = margen + 120;
+  endif
+  
+   if(boolPotencial)
+      uicontrol (f, "style", "text", "string", "Error Potencial", "position",[margen altura 100 30]);      
+      for i = 1:length(x)        
+      uicontrol (f, "style", "text", "string",num2str(erroresPotencial(i)) , "position",[margen (altura-i*50) 80 30]);      
+    endfor
+    uicontrol (f, "style", "text", "string",num2str(sum(erroresPotencial)) , "position",[margen (altura-(length(x)+1)*50) 80 30]);
+    margen = margen + 120;
+  endif
   
 end
